50899b863548b8e074fb22347af8b4539586d1d4
Churche419 <abahiilemuell@gmail.com>
2024-06-23 21:58:35 +0100

    update

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -3,7 +3,7 @@
       <img class="card-img-top" :src="'/src/assets/images/' + product.Image" alt="">
       <div class="card-body">
                 <h2 class="card-title">{{ product.productName }}</h2>
-                <h6 class="card-title">${{ product.price.toFixed(2) }}</h6>
+                <h6 class="card-title">${{ product.price }}</h6>
                 <h6 class="card-text">{{ truncateText(product.product_description, 20) }}</h6>
                 <h6 class="card-text text-muted" style="font-size: 13px;">{{ product.product_category }}</h6>
                 <Router-link :to="{name : 'ProductDetailsView', params:{id:product.id}}">View</Router-link></div>
